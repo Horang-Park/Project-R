217b9538a054c9b6ffacfd2157b0e367107ada3f
HorangPark <horang_park@naver.com>
2025-02-03 00:14:24 +0900

    팝업 및 토스트 추가

--- Assets/Scripts/UI/Game/Fever/FeverMultiplierController.cs
+++ Assets/Scripts/UI/Game/Fever/FeverMultiplierController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Stores;
 using TMPro;
 using UniRx;
@@ -8,6 +9,7 @@ namespace UI.Game.Fever
     public class FeverMultiplierController : MonoBehaviour
     {
         private TMP_Text _text;
+        private Tweener _shakeTweener;
 
         private void Awake()
         {
@@ -23,7 +25,32 @@ namespace UI.Game.Fever
 
         private void MultiplierUpdater(int multiplier)
         {
+            _text.DOScale(1.0f, 0.5f)
+                .From(Vector3.one * 1.2f);
             _text.text = $"x{multiplier * 0.1f:F1}";
+
+            switch (multiplier)
+            {
+                case 20:
+                    Shaking();
+                    break;
+                case 10:
+                    StopShaking();
+                    break;
+            }
+        }
+
+        private void Shaking()
+        {
+            _shakeTweener =  _text.rectTransform.DOShakePosition(1.0f, 5.0f, 15, fadeOut: false)
+                .SetLoops(-1);
+        }
+
+        private void StopShaking()
+        {
+            _shakeTweener.Kill();
+            
+            _text.rectTransform.anchoredPosition = Vector2.zero;
         }
     }
 }
