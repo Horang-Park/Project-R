57f17fe838f9337adda3b5dbb4cc613b1257c8f8
HorangPark <horang_park@naver.com>
2025-02-18 16:21:51 +0900

    랭킹 완료

--- Assets/Scripts/UI/Ranking/RankingInformationPiece.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-namespace UI.Ranking
-{
-    public class RankingInformationPiece : MonoBehaviour
-    {
-
-    }
-}
--- Assets/Scripts/UI/Ranking/RankingUIManager.cs
+++ Assets/Scripts/UI/Ranking/RankingUIManager.cs
@@ -1,16 +1,53 @@
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 namespace UI.Ranking
 {
     public class RankingUIManager : MonoBehaviour
     {
-        private List<KeyValuePair<string, int>> _currentRankingData;
+        [Header("Top Area")]
+        [SerializeField] private Button back;
+        [Header("Scroll Area")]
+        [SerializeField] private GameObject rankingPiecePrefab;
+        [SerializeField] private Transform instantiateTargetParent;
+
+        private readonly List<RankingPiece> _pieces = new();
+
+        private void Awake()
+        {
+            back.onClick.AddListener(() =>
+                FullFadeManager.Instance.FadeOut(() =>
+                {
+                    SceneManager.LoadSceneAsync(1).ToUniTask();
+                }));
+        }
+
+        private void Start()
+        {
+            FullFadeManager.Instance.FadeIn();
+        }
 
         public void InjectData(List<KeyValuePair<string, int>> data)
         {
-            _currentRankingData = data.OrderBy(kv => kv.Value).ToList();
+            Array.Sort(data.ToArray(), (pair, valuePair) => valuePair.Value.CompareTo(pair.Value));
+
+            for (var instantiateTimes = _pieces.Count; instantiateTimes < data.Count; instantiateTimes++)
+            {
+                var component = Instantiate(rankingPiecePrefab, instantiateTargetParent).GetComponent<RankingPiece>();
+
+                component.Initialize();
+
+                _pieces.Add(component);
+            }
+
+            for (var rank = 0; rank < data.Count; rank++)
+            {
+                _pieces[rank].InjectionData(rank, data[rank].Key, data[rank].Value);
+            }
         }
     }
 }
